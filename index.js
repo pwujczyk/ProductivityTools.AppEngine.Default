4658c0e9aec5dacc5913fa62d054bb44ae3e7457
Pawel Wujczyk <pwujczyk@gmail.com>
2026-04-07 22:59:53 +0200

    cloud build

--- index.js
+++ index.js
@@ -3,24 +3,10 @@ const os = require('os');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  
-  const currentDate = new Date().toISOString();
-  
-  let ipAddress = 'Unknown';
-  const networkInterfaces = os.networkInterfaces();
-  for (const interfaceName in networkInterfaces) {
-    for (const iface of networkInterfaces[interfaceName]) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        ipAddress = iface.address;
-        break;
-      }
-    }
-    if (ipAddress !== 'Unknown') {
-      break;
-    }
-  }
 
-  res.end(`Current Date: ${currentDate}\nServer IP Address: ${ipAddress}\n`);
+  const currentDate = new Date().toISOString();
+
+  res.end(`Hello this is pwujczyk default appengine. Current Date: ${currentDate}\n`);
 });
 
 const PORT = process.env.PORT || 8080;
